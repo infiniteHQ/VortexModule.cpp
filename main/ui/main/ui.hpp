b100c24bb98b7d6254b5eda8445fb910aeedace6
diegomrno <diegomoreno.pro45@gmail.com>
2025-07-04 10:35:46 +0200

    update: Vortex + fix module ui

--- main/ui/main/ui.hpp
+++ main/ui/main/ui.hpp
@@ -1,32 +1,29 @@
 #pragma once
+#include "../../../lib/vortex/lib/cherry/cherry.hpp"
 #include "../../../lib/vortex/main/include/vortex.h"
 #include "../../../lib/vortex/main/include/vortex_internals.h"
-#include "../../../lib/vortex/lib/cherry/kit/v1/components/buttons/image_buttons.hpp"
-#include "../../../lib/vortex/lib/cherry/kit/v1/components/buttons/image_text_buttons.hpp"
-#include "../../../lib/vortex/lib/cherry/kit/v1/components/buttons/dropdown_buttons.hpp"
 
 #ifndef UI_HPP
 #define UI_HPP
 
 using namespace Cherry;
 
-class SampleAppWindow : public std::enable_shared_from_this<SampleAppWindow>
-{
+class SampleAppWindow : public std::enable_shared_from_this<SampleAppWindow> {
 public:
-    SampleAppWindow(const std::string &name);
+  SampleAppWindow(const std::string &name);
 
-    void OnImGuiRender();
-    void menubar();
+  void OnImGuiRender();
+  void menubar();
 
-    std::shared_ptr<Cherry::AppWindow> &GetAppWindow();
-    static std::shared_ptr<SampleAppWindow> Create(const std::string &name);
-    void SetupRenderCallback();
-    void Render();
+  std::shared_ptr<Cherry::AppWindow> &GetAppWindow();
+  static std::shared_ptr<SampleAppWindow> Create(const std::string &name);
+  void SetupRenderCallback();
+  void Render();
 
 private:
-    VxContext *ctx;
-    bool opened;
-    std::shared_ptr<AppWindow> m_AppWindow;
+  VxContext *ctx;
+  bool opened;
+  std::shared_ptr<AppWindow> m_AppWindow;
 };
 
 #endif // LOGUTILITY_H
--- scripts/test_project.sh
+++ scripts/test_project.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-mkdir ../lib/vortex/tests/projects/sampleproject/.vx/modules
-cp -r ../dist/* ../lib/vortex/tests/projects/sampleproject/.vx/modules
+mkdir ../lib/vortex/tests/project/.vx/modules
+cp -r ../dist/* ../lib/vortex/tests/project/.vx/modules
 
 VERSION=$(cat ../lib/vortex/version.conf)
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
 VORTEX_PATH="${SCRIPT_DIR}/../lib/vortex/build/dist/${VERSION}/bin/"
-PROJECT_PATH="${SCRIPT_DIR}/../lib/vortex/tests/projects/sampleproject"
+PROJECT_PATH="${SCRIPT_DIR}/../lib/vortex/tests/project"
 
 SESSION_ID="editor-$(date +%m-%d-%Y-%H-%M-%S)-$(shuf -i 1000-9999 -n 1)"
 
